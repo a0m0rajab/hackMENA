f630fbc57acb6abdcc769529de11afe4d9c4bf3e
abdurrahman rajab <rajab-2@windowslive.com>
2020-09-30 20:02:13 +0300

    Add data maps

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,9 +26,7 @@ export default {
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
-
+  plugins: [{ src: '~/plugins/datamaps.js', mode: "client", ssr: false },],
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
@@ -58,7 +56,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
